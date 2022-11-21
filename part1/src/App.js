eb28f8c5ad681aeb8c69a4cf55e9e4114d57df16
josheng <engch28@gmail.com>
2022-11-21 23:42:35 +0800

    part 1a completed

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+// this is a react component
+// ES6 arrow function
+// returns html div tags BUT it is actually JSX
 
-function App() {
+// const App = () => {
+//   console.log('Hello from component')
+//   return (
+//     <div>
+//       <p>Hello world</p>
+//     </div>
+//   )
+// }
+
+// allow dynamic value inside curly brackets
+
+// const App = () => {
+//   const now = new Date()
+//   const a = 10
+//   const b = 20
+
+//   return (
+//     <div>
+//       <p>Hello world, it is {now.toString()}</p>
+//       <p>
+//         {a} plus {b} is {a + b}
+//       </p>
+//     </div>
+//   )
+// }
+
+// const Hello = () => { return (<div>      <p>Hello world</p>    </div>) }
+
+// can include props to pass data into components
+// set the props accordingly to pass relevant data
+const Hello = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+// calling Hello as a JSX div tag
+// component can be reused
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+//       <Hello />
+//       <Hello />
+//       <Hello />
+//     </div>
+//   )
+// }
+
+// example below using variables inside curly brackets and hardcoding.
+// component tag need to be Capitalized
+// must wrap element in a tag
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
+
+// can wrap elements returned by components with an empty element
+
+// const App = () => {
+//   const name = 'Peter'
+//   const age = 10
+
+//   return (
+//     <>
+//       <h1>Greetings</h1>
+//       <Hello name='Maya' age={26 + 10} />
+//       <Hello name={name} age={age} />
+//       <Footer />
+//     </>
+//   )
+// }
+export default App
