5aec1edad477d2c369be9c6742ec830b019458cc
josheng <engch28@gmail.com>
2022-11-26 23:48:44 +0800

    part 1d section 4

--- part1/src/App.js
+++ part1/src/App.js
@@ -32,14 +32,14 @@
 
 // can include props to pass data into components
 // set the props accordingly to pass relevant data
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old      </p>
-    </div>
-  )
-}
+// const Hello = (props) => {
+//   return (
+//     <div>
+//       <p>
+//         Hello {props.name}, you are {props.age} years old      </p>
+//     </div>
+//   )
+// }
 
 // calling Hello as a JSX div tag
 // component can be reused
@@ -58,17 +58,17 @@ const Hello = (props) => {
 // example below using variables inside curly brackets and hardcoding.
 // component tag need to be Capitalized
 // must wrap element in a tag
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
-  )
-}
+// const App = () => {
+//   const name = 'Peter'
+//   const age = 10
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+//       <Hello name='Maya' age={26 + 10} />
+//       <Hello name={name} age={age} />
+//     </div>
+//   )
+// }
 
 // can wrap elements returned by components with an empty element
 
@@ -85,4 +85,91 @@ const App = () => {
 //     </>
 //   )
 // }
+
+// const Hello = ({ name, age }) => { // destructure props into the 2 variable
+//   const bornYear = () => new Date().getFullYear() - age
+
+//   return (
+//     <div>
+//       <p>
+//         Hello {name}, you are {age} years old
+//       </p>
+//       <p>So you were probably born in {bornYear()}</p>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   const name = 'Peter'
+//   const age = 10
+
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+//       <Hello name="Maya" age={26 + 10} />
+//       <Hello name={name} age={age} />
+//     </div>
+//   )
+// }
+
+// const App = (props) => {
+//   const { counter } = props
+//   return (
+//     <div>{counter}</div>
+//   )
+// }
+
+
+// this part is on
+// destruction props so to refactor code
+// usestate function to re-render screen
+// building component to be reused i.e button
+
+// import { useState } from 'react'
+
+// const Display = ({ counter }) => <div>{counter}</div>
+
+// const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+// const App = () => {
+//   const [counter, setCounter] = useState(0)
+
+//   const increaseByOne = () => setCounter(counter + 1)
+//   const decreaseByOne = () => setCounter(counter - 1)
+//   const setToZero = () => setCounter(0)
+
+//   return (
+//     <div>
+//       <Display counter={counter} />
+//       <Button onClick={increaseByOne} text='plus' />
+//       <Button onClick={setToZero} text='zero' />
+//       <Button onClick={decreaseByOne} text='minus' />
+//     </div>
+//   )
+// }
+
+import { useState } from 'react'
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>    </div>
+  )
+}
+
 export default App
--- part1/src/index.js
+++ part1/src/index.js
@@ -3,5 +3,4 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-// this calls the component to render the content
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
